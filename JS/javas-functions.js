402b4b64c81cfea7412602d8b882ecac41b2052f
Ana26.IES <anadelpinoojedadiaz@alumno.ieselrincon.es>
2026-03-16 14:04:26 +0100

    Fase 5: El anterior y este. Aqui está la explicacion del uso de java script en el Doc de texto adjunto.

--- JS/javas-functions.js
+++ JS/javas-functions.js
@@ -1,9 +1,10 @@
-window.onload = function() {
-    // Seleccionamos el botón y el banner
+window.onload = function() 
+// Seleccionamos el botón y el banner
+{
     const botonCerrar = document.querySelector('.closebutton');
     const banner = document.getElementById('cookieBanner');
 
-    // Añadimos el evento de clic
+// Añadimos el evento de clic
     botonCerrar.onclick = function() {
         banner.style.display = 'none';
     };
