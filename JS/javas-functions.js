2bfea8b3316b0aa66c072402172a5997d7b4135b
Ana26.IES <anadelpinoojedadiaz@alumno.ieselrincon.es>
2026-03-16 14:01:54 +0100

    cambios en el css +añado java script + igualo footer para todos los html

---
+++ JS/javas-functions.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // Seleccionamos el botón y el banner
+    const botonCerrar = document.querySelector('.closebutton');
+    const banner = document.getElementById('cookieBanner');
+
+    // Añadimos el evento de clic
+    botonCerrar.onclick = function() {
+        banner.style.display = 'none';
+    };
+};
